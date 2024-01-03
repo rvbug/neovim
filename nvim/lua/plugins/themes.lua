1117cd17f4ea92e6876ecbc91da406c4a4ed1a36
Rakesh Venkat <rakesh.venkat@gmail.com>
2024-01-03 09:38:20 +0530

    Create themes.lua

---
+++ nvim/lua/plugins/themes.lua
@@ -0,0 +1,10 @@
+return  {
+                "sainnhe/edge",
+                lazy = false,
+          name = "edge",
+                priority = 1000,
+
+        config = function()
+                        vim.cmd("colorscheme edge")
+          end
+}
