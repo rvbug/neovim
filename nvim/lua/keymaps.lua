b5420093619434b456c32a5677eb2e8354c0ede5
Rakesh Venkat <rakesh.venkat@gmail.com>
2024-01-03 09:07:08 +0530

    Create keymaps.lua

---
+++ nvim/lua/keymaps.lua
@@ -0,0 +1,14 @@
+--vim.opt.number= true
+--vim.opt.relativenumber=true
+vim.g.mapleader = " "
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.opt.termguicolors = true
+
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.cmd("set tabstop=2")
+vim.cmd("set expandtab")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
