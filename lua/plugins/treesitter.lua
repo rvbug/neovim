9d871b5c0ec3f4db0cabde77e27c31b13484be77
Rakesh Venkat <rakesh.venkat@gmail.com>
2024-01-18 17:35:50 +0530

    treesitter incremental selection

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,11 +3,35 @@ return {
 	build = ":TSUpdate",
 	config = function()
 		local config = require("nvim-treesitter.configs")
-		config.setup({
-			--auto_install = true,
-			ensure_installed = { "lua", "javascript", "rust", "python", "html", "css", "vimdoc", "luadoc", "markdown" },
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
+		config.setup(
+			{
+				--auto_install = true,
+				ensure_installed = {
+					"lua",
+					"javascript",
+					"rust",
+					"python",
+					"html",
+					"css",
+					"vimdoc",
+					"luadoc",
+					"markdown",
+				},
+				highlight = { enable = true },
+				indent = { enable = true },
+			},
+
+			require("nvim-treesitter.configs").setup({
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "gnn",
+						node_incremental = "grn",
+						scope_incremental = "grc",
+						node_decremental = "grm",
+					},
+				},
+			})
+		)
 	end,
 }
