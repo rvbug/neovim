e8839a1b7ec33b538651b0552f8cf6c47bcf5aa9
Rakesh Venkat <rakesh.venkat@gmail.com>
2024-01-18 17:31:28 +0530

    additional help

--- init.lua
+++ init.lua
@@ -1,6 +1,10 @@
 -- adding this keymaps 
 require("keymaps")
 
+
+
+-- setup lazy and look for plugin folder
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
