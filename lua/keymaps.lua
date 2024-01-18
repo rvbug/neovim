72398b59bedef7fcd7c74e4e849f46d3752a2d70
Rakesh Venkat <rakesh.venkat@gmail.com>
2024-01-18 17:18:33 +0530

    additional keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,17 +1,20 @@
---vim.opt.number= true
---vim.opt.relativenumber=true
 vim.g.mapleader = " "
 
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
-vim.cmd("set number")
+vim.cmd("set number") -- can also use vim.opt.number = true
 vim.cmd("set relativenumber")
 vim.cmd("set tabstop=2")
 vim.cmd("set expandtab")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+
+-- ignore case
+vim.opt.ignorecase = true
+vim.opt.wrap = false
+vim.opt.virtualedit = "block"
 
 -- split the window below & right
 vim.opt.splitbelow = true
