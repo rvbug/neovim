ffc88704320e20cd5365f38c554d64e8171c77b8
Rakesh Venkat <rakesh.venkat@gmail.com>
2024-01-30 09:41:00 +0530

    Fix null-ls out of range error
    Fixed the error by removing black formatter

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -8,9 +8,7 @@ return {
         null_ls.builtins.formatting.prettier,  --js
         null_ls.builtins.diagnostics.eslint_d,   --js
         null_ls.builtins.formatting.black,      --py
-        null_ls.builtins.diagnostics.flake8,   --py
-        -- does not work 
-        -- null_ls.builtins.diagnostics.codelldb  --rs
+        --null_ls.builtins.diagnostics.flake8,   --py
 			},
 		})
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
