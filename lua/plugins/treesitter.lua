5a61a0de74b0eb8820e400a52c6e951022e86140
Rakesh Venkat <rakesh.venkat@gmail.com>
2024-01-31 17:42:51 +0530

    Treesitter for sql

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,40 +1,41 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup(
-			{
-				--auto_install = true,
-				ensure_installed = {
-					"lua",
-					"javascript",
-					"rust",
-					"python",
-					"html",
-					"css",
-					"vimdoc",
-					"luadoc",
-					"markdown",
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local config = require("nvim-treesitter.configs")
+    config.setup(
+      {
+        --auto_install = true,
+        ensure_installed = {
+          "lua",
+          "javascript",
+          "rust",
+          "python",
+          "html",
+          "css",
+          "vimdoc",
+          "luadoc",
+          "markdown",
           "regex",
-          "bash", 
+          "bash",
           "markdown_inline",
-				},
-				highlight = { enable = true },
-				indent = { enable = true },
-			},
+          "sql",
+        },
+        highlight = { enable = true },
+        indent = { enable = true },
+      },
 
-			require("nvim-treesitter.configs").setup({
-				incremental_selection = {
-					enable = true,
-					keymaps = {
-						init_selection = "<leader>gi",
-						node_incremental = "<leader>ni",
-						scope_incremental = "<leader>si",
-						node_decremental = "<leader>nd",
-					},
-				},
-			})
-		)
-	end,
+      require("nvim-treesitter.configs").setup({
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<leader>gi",
+            node_incremental = "<leader>ni",
+            scope_incremental = "<leader>si",
+            node_decremental = "<leader>nd",
+          },
+        },
+      })
+    )
+  end,
 }
