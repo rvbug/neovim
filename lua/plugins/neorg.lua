a04b8f52b200f36726194f5b020c26010c64b6c0
Rakesh Venkat <rakesh.venkat@gmail.com>
2024-01-31 15:51:40 +0530

    add neorg

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -14,6 +14,7 @@ return {
                 notes = "$HOME/neorg/notes",
               },
               default_workspace = "notes",
+              index = "index.norg"
             },
           },
         },
