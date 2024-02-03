da5e67e9c95a8233c2d14cde81e29d38be48c165
Rakesh Venkat <rakesh.venkat@gmail.com>
2024-02-03 08:37:10 +0530

    Add ZenMode & Twilight
    Added Zen mode and Twilight for focusing

---
+++ lua/plugins/zen.lua
@@ -0,0 +1,19 @@
+return {
+
+	{
+		"folke/zen-mode.nvim",
+		opts = {},
+	},
+	{
+		"folke/twilight.nvim",
+		opts = {},
+	},
+
+	config = function()
+		-- require("zen-mode").toggle({
+		--   window = {
+		--     width = 0.85,
+		--   }
+		-- })
+	end,
+}
