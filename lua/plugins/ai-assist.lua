cc62f4196b5881292a2ea716d081c1ecb84be39e
Rakesh Venkat <rakesh.venkat@gmail.com>
2024-04-04 20:09:02 +0530

    experimenting trouble pkg
    experimenting trouble packages

--- lua/plugins/ai-assist.lua
+++ lua/plugins/ai-assist.lua
@@ -1,7 +1,8 @@
 return {
-  "Exafunction/codeium.vim"
+  "Exafunction/codeium.vim",
+
 	-- "TabbyML/vim-tabby",
 	-- config = function()
 	-- 	vim.g.tabby_keybinding_accept = "<Tab>"
-	-- end,
+	 -- end,
 }
