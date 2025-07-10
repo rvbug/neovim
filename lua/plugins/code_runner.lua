0fad1b8fc86d72e6e0f037996a8f2e1ff71afa1c
rvbug <rakesh.venkat@gmail.com>
2025-07-10 16:44:44 +0530

    Add py code runner

---
+++ lua/plugins/code_runner.lua
@@ -0,0 +1,10 @@
+
+
+return {
+
+	"CRAG666/code_runner.nvim",
+	config = function()
+		require("lazy").setup()
+	end,
+}
+
---
+++ lua/plugins/color_scheme.lua
@@ -0,0 +1,9 @@
+return {
+  "catgoose/nvim-colorizer.lua",
+    event = "BufReadPre",
+    opts = {},
+
+    config = function()
+      require("colorizer").setup()
+    end
+}
